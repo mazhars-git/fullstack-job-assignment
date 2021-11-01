db85edb992a2c99d79f0bda34cddf2ecc563d577
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-11-01 19:25:59 +0600

    update

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+// const element = <FontAwesomeIcon icon={faCoffee} />
+
+const Dashboard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Dashboard;
